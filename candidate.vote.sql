a41503d3b5e5fe7abe9d00633276aefa9c716dab
twr143 <twr143@gmail.com>
2020-01-14 12:38:01 +0300

    Initial commit

---
+++ candidate.vote.sql
@@ -0,0 +1,19 @@
+create table candidate(id int,Name varchar(2));
+
+insert into candidate values (1,'A');
+insert into candidate values (2,'B');
+insert into candidate values (3,'C');
+insert into candidate values (4,'D');
+insert into candidate values (5,'E');
+
+CREATE TABLE Vote (id int,CandidateId int);
+insert into Vote values(1,2);
+insert into Vote values(2,4);
+insert into Vote values(3,3);
+insert into Vote values(4,2);
+insert into Vote values(5,5);
+
+
+
+--with voteCnt as (select CandidateId as cid, count(CandidateId) as cnt from vote group by cid order by cnt desc limit 1)
+--select c.name,v.cnt from candidate c join voteCnt v on c.id=v.cid
---
+++ max.answer.rate.sql
@@ -0,0 +1,28 @@
+create table survey_log(uid int,action varchar(6), question_id int);
+delete from survey_log;
+insert into survey_log values(5,'show',285);
+insert into survey_log values(5,'answer',285);
+insert into survey_log values(5,'show',369);
+insert into survey_log values(5,'skip',369);
+insert into survey_log values(5,'show',379);
+insert into survey_log values(5,'skip',379);
+insert into survey_log values(9,'show',285);
+insert into survey_log values(9,'answer',285);
+insert into survey_log values(9,'show',369);
+insert into survey_log values(9,'answer',369);
+insert into survey_log values(9,'show',379);
+insert into survey_log values(9,'skip',379);
+
+--max answer rate
+with grouping as (select action, question_id, count(uid) as cnt from survey_log where action <> 'show' group by action, question_id order by question_id)
+,grouping_all_answ as (
+    select question_id from grouping group by question_id having count(action) =1 and max(action) = 'answer'
+    )
+,grouping_none_answ as (
+    select question_id from grouping group by question_id having count(action) =1 and max(action) = 'skip'
+    )
+,grouping_part_answ as (
+    select g.question_id,g.cnt as cnt_a,g2.cnt as cnt_s from grouping g join grouping g2 on g.question_id=g2.question_id and g.action='answer' and g2.action ='skip'
+)
+select question_id, 1.0 as rate from grouping_all_answ union select question_id,0.0 from grouping_none_answ
+union select question_id, cnt_a::real/(cnt_a::real+cnt_s::real) from grouping_part_answ
